25a230e013f8387196ac9b9583d2dc2f5b3faedc
domsmutku <zientek.nataliaa@gmail.com>
2025-09-06 14:21:18 +0200

    Add display forecast function in JS

--- src/javascript.js
+++ src/javascript.js
@@ -83,7 +83,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon">☀️</div>
+              <div class="weather-forecast-temperatures">
+                <div class="weather-forecast-temperature">
+                  <strong>15º</strong>
+                </div>
+                <div class="weather-forecast-temperature">9º</div>
+              </div>
+            </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let forecastElement = document.querySelector("#forecast");
 let searchFormDocument = document.querySelector("#search-form");
 searchFormDocument.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Wrocław");
+displayForecast();
